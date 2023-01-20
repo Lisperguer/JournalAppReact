165cb27a590053db425b8db8270847a3d9d0c755
Lisperguer <felipe.lisperguer.h@gmail.com>
2023-01-20 12:30:25 -0300

    feat(JournalApp): add shell of views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,13 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        {" "}
-        Lorem ipsum as askjdnb
-        ajsdkajsdnkajnsdkandkasjndkasjndaksndkjasndkasjndkajsndanksndakjnsdkandkajnsdkamsdnmsk
-        kj nj kasjdn jn ajs j asjn jksd ka js jdak jka
-      </Typography>
-      {/* NothinSelected */}
-      {/* Note View */}
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
+
     </JournalLayout>
   );
 };
